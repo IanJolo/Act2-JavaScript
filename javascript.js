af49f3e4865ab4c0c12e148e5376f06cd52d28aa
Julian <48520440@est.ort.edu.ar>
2024-10-10 15:51:57 -0300

    vf version1

--- javascript.js
+++ javascript.js
@@ -1,19 +1,20 @@
 function RealizarOperacion(){
     let resultado;
     let pAModificar=document.getElementById("resultado")
-    let numero1=document.getElementById("numero1");
-    let numero2=document.getElementById("numero2");
-    let operacion=document.getElementById("operacion");
+    let numero1=parseInt(document.getElementById("numero1").value);
+    let numero2=parseInt(document.getElementById("numero2").value);
+    let operacion=document.getElementById("operacion")
+
     if(operacion.value=="sumar"){
-        resultado=numero1.value+numero2.value;
+        resultado=numero1 + numero2;
     }else if(operacion.value=="restar"){
-        resultado=numero1.value-numero2.value;
+        resultado=numero1-numero2;
     }
     else if(operacion.value=="multiplicar"){
-        resultado=numero1.value*numero2.value;
+        resultado=(numero1*numero2);
     }
     else if(operacion.value=="dividir"){
-        resultado=numero1.value/numero2.value;
+        resultado=numero1/numero2;
     }
     pAModificar.innerHTML=resultado;
 }
