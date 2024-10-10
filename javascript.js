3995046400ca54ea61f97c0009700fc2aacfd528
Julian <48520440@est.ort.edu.ar>
2024-10-10 15:40:50 -0300

    avances iniciales

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function RealizarOperacion(){
+    let resultado;
+    let pAModificar=document.getElementById("resultado")
+    let numero1=document.getElementById("numero1");
+    let numero2=document.getElementById("numero2");
+    let operacion=document.getElementById("operacion");
+    if(operacion.value=="sumar"){
+        resultado=numero1.value+numero2.value;
+    }else if(operacion.value=="restar"){
+        resultado=numero1.value-numero2.value;
+    }
+    else if(operacion.value=="multiplicar"){
+        resultado=numero1.value*numero2.value;
+    }
+    else if(operacion.value=="dividir"){
+        resultado=numero1.value/numero2.value;
+    }
+    pAModificar.innerHTML=resultado;
+}
